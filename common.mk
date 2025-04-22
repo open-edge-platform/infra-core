c19054f01af605c61ed1d0f837ac128e9f1ba84a
Daniele Moro <daniele.moro@intel.com>
2025-04-22 16:26:07 +0200

    chore: Remove ECR create repository from Makefile

--- common.mk
+++ common.mk
@@ -118,8 +118,6 @@ common-docker-build: ## Build Docker image
 	@rm -rf vendor common.mk version.mk
 
 common-docker-push: ## Tag and push Docker image
-	# TODO: remove ecr create
-	aws ecr create-repository --region us-west-2 --repository-name $(DOCKER_REPOSITORY)/$(DOCKER_SECTION)/$(DOCKER_IMG_NAME) || true
 	docker tag $(DOCKER_IMG_NAME):$(VERSION) $(DOCKER_TAG_BRANCH)
 	docker tag $(DOCKER_IMG_NAME):$(VERSION) $(DOCKER_TAG)
 	docker push $(DOCKER_TAG)
