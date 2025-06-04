9ca91293ed5ace76dbe14ebb7e2b49de4ef73b8d
Niket Kumar <niket.kumar@intel.com>
2025-06-04 16:58:03 +0000

    feat: add edge node custom config API (#152)
    Co-authored-by: Tadeusz Matenko <tadeusz.matenko@intel.com>

--- apiv2/internal/pbapi/resources/compute/v1/compute_constants.pb.go
+++ apiv2/internal/pbapi/resources/compute/v1/compute_constants.pb.go
@@ -138,11 +138,13 @@ const (
 	InstanceResourceFieldExistingCves                      = "existing_cves"
 	InstanceResourceFieldRuntimePackages                   = "runtime_packages"
 	InstanceResourceFieldOsUpdateAvailable                 = "os_update_available"
+	InstanceResourceEdgeCustomConfig                       = "custom_config"
 	InstanceResourceFieldInstanceID                        = "instanceID"
 	InstanceResourceFieldHostID                            = "hostID"
 	InstanceResourceFieldOsID                              = "osID"
 	InstanceResourceFieldLocalAccountID                    = "local_accountID"
 	InstanceResourceFieldOsUpdatePolicyID                  = "os_update_policyID"
+	InstanceResourceFieldCustomConfigID                    = "custom_configID"
 	InstanceResourceEdgeTimestamps                         = "timestamps"
 
 	// Fields and Edges constants for "WorkloadResource"
