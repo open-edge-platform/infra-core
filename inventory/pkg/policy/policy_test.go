f93e80fd87e9cfb089d2dce323bf0e63d6e2e008
ktaube26 <krystian.taube@intel.com>
2025-06-05 08:58:35 +0000

    add missing test cases for desiredState and desiredAmtState

--- inventory/pkg/policy/policy_test.go
+++ inventory/pkg/policy/policy_test.go
@@ -594,6 +594,66 @@ func TestPolicyVerifyCreate(t *testing.T) { // table-driven test
 			resourceID: hostID,
 			valid:      true,
 		},
+		"Test_ClientAPI_Create_Host_Success10": {
+			cliendKind: inv_v1.ClientKind_CLIENT_KIND_API,
+			resource: &inv_v1.Resource{
+				Resource: &inv_v1.Resource_Host{
+					Host: &computev1.HostResource{
+						Name:            "host",
+						SerialNumber:    "12345678",
+						DesiredState:    computev1.HostState_HOST_STATE_ONBOARDED,
+						DesiredAmtState: computev1.AmtState_AMT_STATE_PROVISIONED,
+					},
+				},
+			},
+			resourceID: hostID,
+			valid:      true,
+		},
+		"Test_ClientRM_Create_Host_Success11": {
+			cliendKind: inv_v1.ClientKind_CLIENT_KIND_RESOURCE_MANAGER,
+			resource: &inv_v1.Resource{
+				Resource: &inv_v1.Resource_Host{
+					Host: &computev1.HostResource{
+						Name:            "host",
+						SerialNumber:    "12345678",
+						CurrentState:    computev1.HostState_HOST_STATE_ONBOARDED,
+						CurrentAmtState: computev1.AmtState_AMT_STATE_PROVISIONED,
+					},
+				},
+			},
+			resourceID: hostID,
+			valid:      true,
+		},
+		"Test_ClientAPI_Create_Host_Fail15": {
+			cliendKind: inv_v1.ClientKind_CLIENT_KIND_API,
+			resource: &inv_v1.Resource{
+				Resource: &inv_v1.Resource_Host{
+					Host: &computev1.HostResource{
+						Name:            "host",
+						SerialNumber:    "12345678",
+						CurrentState:    computev1.HostState_HOST_STATE_ONBOARDED,
+						DesiredAmtState: computev1.AmtState_AMT_STATE_PROVISIONED,
+					},
+				},
+			},
+			resourceID: hostID,
+			valid:      false,
+		},
+		"Test_ClientRM_Create_Host_Fail16": {
+			cliendKind: inv_v1.ClientKind_CLIENT_KIND_RESOURCE_MANAGER,
+			resource: &inv_v1.Resource{
+				Resource: &inv_v1.Resource_Host{
+					Host: &computev1.HostResource{
+						Name:            "host",
+						SerialNumber:    "12345678",
+						DesiredState:    computev1.HostState_HOST_STATE_ONBOARDED,
+						CurrentAmtState: computev1.AmtState_AMT_STATE_PROVISIONED,
+					},
+				},
+			},
+			resourceID: hostID,
+			valid:      false,
+		},
 		"Test_ClientAPI_Register_Host_Success1": {
 			cliendKind: inv_v1.ClientKind_CLIENT_KIND_API,
 			resource: &inv_v1.Resource{
@@ -1100,7 +1160,6 @@ func TestPolicyVerifyUpdate(t *testing.T) { // table-driven test
 				Resource: &inv_v1.Resource_Host{
 					Host: &computev1.HostResource{
 						Name:            "host",
-						SerialNumber:    "12345678",
 						CurrentAmtState: computev1.AmtState_AMT_STATE_PROVISIONED,
 					},
 				},
@@ -1114,7 +1173,6 @@ func TestPolicyVerifyUpdate(t *testing.T) { // table-driven test
 				Resource: &inv_v1.Resource_Host{
 					Host: &computev1.HostResource{
 						Name:            "host",
-						SerialNumber:    "12345678",
 						DesiredAmtState: computev1.AmtState_AMT_STATE_PROVISIONED,
 					},
 				},
