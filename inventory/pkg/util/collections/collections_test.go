967c0f92c0529d20ec23b3d2a4e2b183de65e9e2
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2025-06-24 13:29:02 +0530

    Add missed UT file
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

---
+++ inventory/pkg/util/collections/collections_test.go
@@ -0,0 +1,72 @@
+package collections
+
+import (
+	"testing"
+)
+
+func TestConcatMapValuesSorted(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    map[string]string
+		expected string
+	}{
+		{
+			name:     "NilMap",
+			input:    nil,
+			expected: "",
+		},
+		{
+			name:     "EmptyMap",
+			input:    map[string]string{},
+			expected: "",
+		},
+		{
+			name: "SingleKeyValue",
+			input: map[string]string{
+				"a": "foo",
+			},
+			expected: "\"foo\"",
+		},
+		{
+			name: "SingleKeyEmptyValue",
+			input: map[string]string{
+				"a": "",
+			},
+			expected: "",
+		},
+		{
+			name: "MultipleKeysSorted",
+			input: map[string]string{
+				"b": "bar",
+				"a": "foo",
+				"c": "baz",
+			},
+			expected: "\"foo\\x1fbar\\x1fbaz\"",
+		},
+		{
+			name: "KeysWithEmptyValue",
+			input: map[string]string{
+				"a": "",
+				"b": "bar",
+			},
+			expected: "\"bar\"",
+		},
+		{
+			name: "KeysWithInterspersedEmptyValue",
+			input: map[string]string{
+				"a": "foo",
+				"b": "",
+				"c": "baz",
+			},
+			expected: "\"foo\\x1fbaz\"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ConcatMapValuesSorted(tt.input, "\x1f")
+			if got != tt.expected {
+				t.Errorf("ConcatMapValuesSorted() = %q, want %q", got, tt.expected)
+			}
+		})
+	}
+}
