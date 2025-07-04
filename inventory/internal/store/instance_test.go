cd23ec2fe7ddf0e78684ee7bd1c14decf9c17b3f
Niket Kumar <niket.kumar@intel.com>
2025-07-04 06:17:14 +0000

    updated customconfig UT

--- inventory/internal/store/instance_test.go
+++ inventory/internal/store/instance_test.go
@@ -85,6 +85,8 @@ func Test_Create_Get_Delete_Instance(t *testing.T) {
 	oup := dao.CreateOSUpdatePolicy(t, tenantID,
 		inv_testing.OsUpdatePolicyName("OsUpdatePolicy"), inv_testing.OSUpdatePolicyLatest())
 
+	cc := dao.CreateCustomConfig(t, tenantID, "custom-config1", "Test custom config resource", testCloudInitCfg)
+
 	testcases := map[string]struct {
 		in    *computev1.InstanceResource
 		valid bool
@@ -165,6 +167,18 @@ func Test_Create_Get_Delete_Instance(t *testing.T) {
 				DesiredOs:    os,
 				CurrentOs:    os,
 				Localaccount: localaccount,
+			},
+			valid: true,
+		},
+		"CreateGoodMetalInstanceWithCustomConfig": {
+			in: &computev1.InstanceResource{
+				TenantId:     tenantID,
+				Kind:         computev1.InstanceKind_INSTANCE_KIND_METAL,
+				DesiredState: computev1.InstanceState_INSTANCE_STATE_RUNNING,
+				Host:         host,
+				DesiredOs:    os,
+				CurrentOs:    os,
+				CustomConfig: []*computev1.CustomConfigResource{cc},
 			},
 			valid: true,
 		},
@@ -395,6 +409,8 @@ func Test_UpdateInstance(t *testing.T) {
 	localaccount := inv_testing.CreateLocalAccount(t,
 		"test-user",
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtu+7Pdtj6ihyFynecnd+155AdxqvHhMRxvxdcQ8/D/ test-user1@example.com")
+
+	cc := inv_testing.CreateCustomConfig(t, "custom-config1", "Test custom config resource", testCloudInitCfg)
 
 	// create Instance to update
 	createresreq := &inv_v1.Resource{
@@ -662,6 +678,21 @@ func Test_UpdateInstance(t *testing.T) {
 				Paths: []string{
 					instanceresource.FieldCurrentState,
 					instanceresource.EdgeLocalaccount,
+				},
+			},
+			resourceID:   instanceResID,
+			valid:        false,
+			expErrorCode: codes.InvalidArgument,
+		},
+		"UpdateInstanceCustomConfigFail": {
+			in: &computev1.InstanceResource{
+				CurrentState: computev1.InstanceState_INSTANCE_STATE_RUNNING,
+				CustomConfig: []*computev1.CustomConfigResource{cc},
+			},
+			fieldMask: &fieldmaskpb.FieldMask{
+				Paths: []string{
+					instanceresource.FieldCurrentState,
+					instanceresource.EdgeCustomConfig,
 				},
 			},
 			resourceID:   instanceResID,
