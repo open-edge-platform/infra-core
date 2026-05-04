a8112231510d32f4cc9850e3ebeb7ccf54be503e
Gupta, Gunjan <gunjan.gupta@intel.com>
2026-05-04 07:22:01 -0700

    nexus-replacement: remove nexus stub files from tenant-controller (replaced by internal/tenancy)

--- tenant-controller/internal/nexus/client.go
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
-// Package nexus is replaced by internal/tenancy. See tenancy/tenancy-hook.go.
-package nexus
--- tenant-controller/internal/nexus/client_test.go
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
-// Tests removed: nexus package is replaced by internal/tenancy. See tenancy/tenancy-hook.go.
-package nexus
