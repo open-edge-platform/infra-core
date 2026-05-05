9e2b39a196a8e93142c5c2d939a359475cfd5672
Gupta, Gunjan <gunjan.gupta@intel.com>
2026-05-04 21:39:40 -0700

    nexus-replacement: fix globalLogLevel flag in datamodel test package

--- tenant-controller/internal/datamodel/main_test.go
+++ tenant-controller/internal/datamodel/main_test.go
@@ -3,3 +3,15 @@
 
 // Tests removed: datamodel package is replaced by internal/tenancy. See tenancy/tenancy-hook.go.
 package datamodel
+
+import (
+	"os"
+	"testing"
+
+	// Import logging to register globalLogLevel flag used by the test runner.
+	_ "github.com/open-edge-platform/infra-core/inventory/v2/pkg/logging"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
