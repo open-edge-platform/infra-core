ec4d1f8d37e9a02b8266f2b2dba6c16bd9d30c2b
Gupta, Gunjan <gunjan.gupta@intel.com>
2026-05-13 00:36:30 -0700

    cleanup and version bump

--- tenant-controller/internal/datamodel/datamodel_test.go
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
-// Tests removed: datamodel package is replaced by internal/tenancy. See tenancy/tenancy-hook.go.
-package datamodel
